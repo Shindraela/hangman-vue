73c79ce9724df4e3ffed2878117d00d2535742e1
Shindraela <farooq.asmaa@gmail.com>
2022-07-23 00:53:03 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="word">
+    <span v-for="(letter, idx) in word" :key="idx">
+      <Word :letter="letter" :visible="getVisibleLetter(letter)" />
+    </span>
+  </div>
+
+  <button type="button" :disabled="lettersDisabled.includes(letter)" v-for="(letter, index) in letters" :key="index" v-on:click="chooseLetter(letter)">{{ letter }}</button>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Word from './components/Word.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+@Options({
+  components: { Word },
+})
+export default class App extends Vue {
+  public letters: string[] = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  private words: string[] = ['ARTICHOKE','EGGPLANT','ASPARAGUS','CHICKPEAS','PEANUTS','PEAS','BROCCOFLOWER','BROCCOLI','CABBAGE','CAULIFLOWER','CELERY','ENDIVE','FIDDLEHEADS','FRISEE','FENNEL','KALE','SPINACH','ANISE','BASIL','CARAWAY','BASIL','CORIANDER','CHAMOMILE','OREGANO','PARSLEY','ROSEMARY']
+  public matchLetters: string[] = []
+  public lettersDisabled: string[] = []
+  get generateWord(): string {
+    return this.words[Math.floor(Math.random() * this.words.length)]
+  }
+  public word: string = this.generateWord
+  public visible: boolean = false
+  chooseLetter(letter: string): string {
+    this.matchLetters.push(letter)
+    this.lettersDisabled.push(letter)
+    return letter
+  }
+  getVisibleLetter(letter: string): boolean {
+    this.visible = this.matchLetters.includes(letter)
+    return this.visible
   }
 }
 </script>
@@ -22,5 +45,19 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.word {
+  margin-bottom: 24px;
+}
+
+button {
+  margin: 6px;
+  font-size: 24px;
+}
+
+span {
+  margin-right: 10px;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/Word.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>{{ visible ? letter : '_' }}</span>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component"
+
+@Options({
+  props: {
+    letter: String,
+    visible: Boolean
+  }
+})
+
+export default class Word extends Vue {
+  letter!: string
+  visible!: boolean
+}
+</script>
+
+<style scoped>
+  span {
+    font-size: 24px;
+  }
+</style>
